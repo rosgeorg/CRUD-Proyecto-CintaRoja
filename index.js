51dac61e383b2fd382cf66fd75ae05067c2fa585
Jorge Salas <js.2701e@gmail.com>
2020-04-16 19:48:41 -0500

    CORS added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('./database/mongoClient');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hola Mundo');
@@ -13,3 +15,5 @@ app.get('/', (req, res) => {
 app.use('/api/v1', require('./router'));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+console.log('CORS-enabled web server listening on port 80');
